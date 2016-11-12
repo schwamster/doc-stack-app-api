6111b6447c74c600f9ad532c3f239f9809e0ac95
schwamster <bastian.toepfer@bodoconsult.de>
2016-11-12 11:52:58 +0100

    upload controller updated

--- src/doc-stack-app-api/Controllers/UploadController.cs
+++ src/doc-stack-app-api/Controllers/UploadController.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
 using System.IO;
 using doc_stack_app_api.Store;
+using System;
+using System.Net.Http;
+using System.Globalization;
+using System.Text.RegularExpressions;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -44,11 +48,9 @@ namespace docstackapp.Controllers
             foreach (var f in file)
             {
                 if (!allowedContentTypes.Contains(f.ContentType))
-                {
+                {   
                     return false;
                 }
-
-                
 
                 if (f.Length > 0)
                 {
@@ -57,11 +59,11 @@ namespace docstackapp.Controllers
                     var client = "dummyClient";
                     var bytes = ConvertToBytes(f);
                     var stringRepresentationOfFile = System.Convert.ToBase64String(bytes);
-                    
+
                     this.logger.LogInformation("Adding document to queue");
-                    
+
                     //adding doc to store
-                    AddToStore(user, client, documentId, f);
+                    await AddToStore(user, client, documentId, f, this.config["StoreHostName"]);
 
                     //adding doc to queue for further processing
                     var payload = $"{{\"id\":\"{documentId}\",\"name\":\"{f.FileName}\", \"size\":{f.Length}, \"user\":\"{user}\", \"client\":\"{client}\", \"content\":\"{stringRepresentationOfFile}\"}}";
@@ -72,7 +74,36 @@ namespace docstackapp.Controllers
             return result;
         }
 
-        internal byte[] ConvertToBytes(IFormFile image)
+        public static async Task<bool> AddToStore(string user, string clientName, Guid documentId, IFormFile f, string uploadHost)
+        {
+            byte[] document = ConvertToBytes(f);
+            using (var client = new HttpClient())
+            {
+                using (var content = new MultipartFormDataContent("Upload----" + DateTime.Now.ToString(CultureInfo.InvariantCulture)))
+                {
+                    var values = new[]
+                    {
+                        new KeyValuePair<string, string>("user", user),
+                        new KeyValuePair<string, string>("client", clientName),
+                        new KeyValuePair<string, string>("id", documentId.ToString())
+                    };
+
+                    foreach (var keyValuePair in values)
+                    {
+                        content.Add(new StringContent(keyValuePair.Value), keyValuePair.Key);
+                    }
+
+                    content.Add(new StreamContent(new MemoryStream(document)), "file", f.FileName);
+                    using (var message = await client.PostAsync($"{uploadHost}/api/Document", content))
+                    {
+                        var input = await message.Content.ReadAsStringAsync();
+                        return true;
+                    }
+                }
+            }
+        }
+
+        internal static byte[] ConvertToBytes(IFormFile image)
         {
             byte[] bytes = null;
             BinaryReader reader = new BinaryReader(image.OpenReadStream());
@@ -86,7 +117,7 @@ namespace docstackapp.Controllers
             queue.AddItem(key, value);
         }
 
-        
-        
+
+
     }
 }
