ba707e0995aab3c3ef6295d6e95b71490e9ec6ce
schwamster <bastian.toepfer@bodoconsult.de>
2016-11-23 21:10:57 +0100

    test results are moved to circle testreport folder

--- build-container.sh
+++ build-container.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 set -e
 dotnet restore
-dotnet test test/doc-stack-app-api.tests/project.json -xml $(pwd)/testresults.xml
+dotnet test test/doc-stack-app-api.tests/project.json -xml $(pwd)/testresults/out.xml
 rm -rf $(pwd)/publish/web
 dotnet publish src/doc-stack-app-api/project.json -c release -o $(pwd)/publish/web
