fed8f38d1dcbd0ef73cecf4ec837cf725fd2ad22
schwamster <bastian.toepfer@gmail.com>
2016-11-23 14:56:18 +0100

    updated names and fixed issues in sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
-#!bin/bash
-set -e
-dotnet restore
-# dotnet test test/folder/project.json
-rm -rf $(pwd)/publish/web
-dotnet publish src/doc-stack-app-api/project.json -c release -o $(pwd)/publish/web
+# build.sh
+
+#!/bin/bash
+
+docker-compose -f docker-compose-build.yml up
