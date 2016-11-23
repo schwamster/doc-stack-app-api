db04dc870382263659d170bcd65d807d5b4c17e4
schwamster <bastian.toepfer@gmail.com>
2016-11-23 19:54:12 +0100

    changed build image

---
+++ test/doc-stack-app-api.tests/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using doc_stack_app_api.Store;
+using Xunit;
+
+namespace Tests
+{
+    public class RedisQueueServiceTest
+    {
+         private readonly RedisQueueService _redisService;
+        //  public RedisQueueServiceTest()
+        //  {
+        //      _redisService = new RedisQueueService(null, null);
+        //  }
+
+        [Fact]
+        public void RedisQueueService_GivenLocalhost_ReturnsExpectedIp()
+        {
+            var result = RedisQueueService.GetIp("localhost");
+            Assert.Equal("127.0.0.1", result);
+        }
+    }
+}
