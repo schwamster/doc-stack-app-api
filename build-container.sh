19d3f1a548c829069b67c87fd1aae2cbfde16066
schwamster <bastian.toepfer@bodoconsult.de>
2016-11-23 21:01:34 +0100

    test now ouput results

--- build-container.sh
+++ build-container.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 set -e
 dotnet restore
-dotnet test test/doc-stack-app-api.tests/project.json
+dotnet test test/doc-stack-app-api.tests/project.json -xml $(pwd)/testresults.xml
 rm -rf $(pwd)/publish/web
 dotnet publish src/doc-stack-app-api/project.json -c release -o $(pwd)/publish/web
