5cc184f06c702be8e4837aaddde2208dd6e0e964
schwamster <bastian.toepfer@bodoconsult.de>
2017-03-09 16:46:20 +0100

    typo

--- build-container.sh
+++ build-container.sh
@@ -2,6 +2,6 @@
 set -e
 dotnet restore
 dotnet test test/doc-stack-app-api.tests/doc-stack-app-api.tests.csproj --logger:trx
-cp -a test/doc-stack-app-api.tests/testresults/. $(pwd)/testresults
+cp -a test/doc-stack-app-api.tests/TestResults/. $(pwd)/testresults
 rm -rf $(pwd)/publish/web
 dotnet publish src/doc-stack-app-api/doc-stack-app-api.csproj -c release -o $(pwd)/publish/web
