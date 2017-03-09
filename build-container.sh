decbc814c710aed0fc837e5bc07d9f936954c238
schwamster <bastian.toepfer@bodoconsult.de>
2017-03-09 07:27:12 +0100

    upgrade from 1.1.0 to 1.1.1 up to vs17 project.json to csproj

--- build-container.sh
+++ build-container.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 set -e
 dotnet restore
-dotnet test test/doc-stack-app-api.tests/project.json -xml $(pwd)/testresults/out.xml
+dotnet test test/doc-stack-app-api.tests/doc-stack-app-api.tests.csproj -xml $(pwd)/testresults/out.xml
 rm -rf $(pwd)/publish/web
-dotnet publish src/doc-stack-app-api/project.json -c release -o $(pwd)/publish/web
+dotnet publish src/doc-stack-app-api/doc-stack-app-api.csproj -c release -o $(pwd)/publish/web
